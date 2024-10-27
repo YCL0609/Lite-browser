aaa4aa2eb4c2d26458ecaff2907310ca49c43ec6
YCL0609 <YCL20060609@163.com>
2024-10-27 02:36:43 -0300

    Update to version 1.6

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Lite-browser
-A browser for lightweight web environments
-# Wait for update
+适用于轻量级 Web 环境的浏览器.<br>
+A browser for lightweight web environments.<br>
+## Note
+如果需要自行编译请修改main.js和Tools/menu.js将引用从开发模式转换为日常模式<br>
+If you need to compile yourself, modify main.js and Tools/menu.js to convert references from dev mode to everyday mode<br>
+例如(e.g.):
+```javascript
+/****** 当前模式为开发模式模式(The current mode is development mode) ******/
+const menu_tool = require('./Tools/menu').default;
+// const menu_tool = require('../Tools.asar/menu').default;
+/****** 当前模式为日常模式(The current mode is Everyday mode) ******/
+// const menu_tool = require('./Tools/menu').default;
+const menu_tool = require('../Tools.asar/menu').default;
+```
--- main.js
+++ main.js
@@ -1,114 +1,108 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron');
+const menu_tool = require('./Tools/menu').default;
+// const menu_tool = require('../Tools.asar/menu').default;
 
-// 设置用户配置文件夹
-app.setPath('userData', process.cwd() + "\\resources\\Data");
+// 菜单-编辑
+const menu_edit = [{
+  label: '编辑...',
+  submenu: [{
+    label: '撤销',
+    accelerator: 'Ctrl+Z',
+    role: 'undo'
+  }, {
+    label: '重做',
+    accelerator: 'Ctrl+Y',
+    role: 'redo'
+  }, {
+    label: '全选',
+    accelerator: 'Ctrl+A',
+    role: 'selectall'
+  }, {
+    type: 'separator'
+  }, {
+    label: '剪切',
+    accelerator: 'Ctrl+X',
+    role: 'cut'
+  }, {
+    label: '复制',
+    accelerator: 'Ctrl+C',
+    role: 'copy'
+  }, {
+    label: '粘贴',
+    accelerator: 'Ctrl+V',
+    role: 'paste'
+  }, {
+    label: '粘贴为纯文本',
+    accelerator: 'CmdOrCtrl+Shift+V',
+    role: 'pasteAndMatchStyle'
+  }]
+}]
 
+// 菜单-页面
+const menu_page = [{
+  label: '刷新',
+  accelerator: 'F5',
+  role: 'reload'
+}, {
+  label: '忽略缓存刷新',
+  accelerator: 'shift+F5',
+  role: 'forceReload'
+}, {
+  label: '在独立线程中打开',
+  accelerator: 'F9',
+  click: () => {
+    const url = BrowserWindow.getFocusedWindow().webContents.getURL();
+    const newwin = new BrowserWindow({ width: 1024, height: 600 });
+    newwin.loadURL(url);
+  }
+}, {
+  label: '显示当前网址',
+  accelerator: 'F10',
+  click: () => {
+    const url = BrowserWindow.getFocusedWindow().webContents.getURL();
+    const newwin = new BrowserWindow({ width: 500, height: 200 });
+    newwin.setMenu(null);
+    newwin.loadURL('data:text/html,<meta charset="UTF-8"><title>当前网址</title><body style=word-wrap:break-word;word-break:break-all>' + url)
+  }
+}, {
+  label: '全屏',
+  accelerator: 'F11',
+  role: 'toggleFullScreen'
+
+}, {
+  label: '开发者工具',
+  accelerator: 'F12',
+  role: 'toggleDevTools'
+}];
+
+app.setPath('userData', process.cwd() + "\\resources\\Data");// 用户配置文件夹
 app.whenReady().then(() => {
   const win = new BrowserWindow({
-    // 设置主窗口初始尺寸
     width: 1024,
     height: 600,
-    // 设置主窗口最小尺寸
     minWidth: 1024,
-    minHeight: 600,
+    minHeight: 600
   })
-  // 加载主页
   win.loadFile('index.html');
 
-  // 自定义菜单
-  const menuTemplate = [{
-    label: '编辑...',
-    submenu: [{
-      label: '剪切',
-      accelerator: 'Ctrl+X',
-      role: 'cut'
-    }, {
-      label: '复制',
-      accelerator: 'Ctrl+C',
-      role: 'copy'
-    }, {
-      label: '粘贴',
-      accelerator: 'Ctrl+V',
-      role: 'paste'
-    }, {
-      label: '粘贴为纯文本',
-      accelerator: 'CmdOrCtrl+Shift+V',
-      role: 'pasteAndMatchStyle'
-    }, {
-      type: 'separator'
-    }, {
-      label: '撤销',
-      accelerator: 'Ctrl+Z',
-      role: 'undo'
-    }, {
-      label: '全选',
-      accelerator: 'Ctrl+A',
-      role: 'selectall'
-    },]
-  }, {
-    label: '刷新 (F5)',
-    accelerator: 'F5',
-    role: 'reload'
-  }, {
-    label: '忽略缓存刷新 (shift+F5)',
-    accelerator: 'shift+F5',
-    role: 'forceReload'
-  }, {
-    label: '全屏 (F11)',
-    accelerator: 'F11',
-    role: 'toggleFullScreen'
-  }, {
-    label: '开发者工具 (F12)',
-    accelerator: 'F12',
-    role: 'toggleDevTools'
-  }, {
-    label: '显示当前网址 (F10)',
-    accelerator: 'F10',
-    click: () => {
-      const currentWindow = BrowserWindow.getFocusedWindow();
-      url = currentWindow.webContents.getURL();
-      const newwin = new BrowserWindow({
-        width: 500,
-        height: 200,
-      })
-      newwin.setMenu(null);
-      newwin.loadURL('data:text/html,<meta charset="UTF-8"><title>当前网址</title><body style="word-wrap:break-word;word-break:break-all">' + url);
-      // newwin.loadURL(url);
-    }
-  }, {
-    label: '在独立进程中打开 (F9)',
-    accelerator: 'F9',
-    click: () => {
-      const currentWindow = BrowserWindow.getFocusedWindow();
-      url = currentWindow.webContents.getURL();
-      const newwin = new BrowserWindow({
-        width: 1024,
-        height: 600,
-      })
-      newwin.loadURL(url);
-    }
-  }, {
-    label: '关闭菜单(全局)',
-    click: () => {
-      Menu.setApplicationMenu(null);
-    }
-  }, {
-    label: '笔记',
-    click: () => {
-      const newwin = new BrowserWindow({
-        width: 500,
-        height: 200,
-      })
-      newwin.setMenu(null);
-      newwin.loadURL('data:text/html,<body contenteditable style=line-height:1.5;font-size:20px>')
-    }
-  }
-  ]
+  // 标题栏菜单
+  const titleMenu = Menu.buildFromTemplate(
+    menu_edit
+      .concat(menu_tool)
+      .concat(menu_page)
+      .concat({ label: '关闭菜单(全局)', click: () => { Menu.setApplicationMenu(null) } })
+  )
+  Menu.setApplicationMenu(titleMenu);
 
-  const menuBuilder = Menu.buildFromTemplate(menuTemplate);
-  Menu.setApplicationMenu(menuBuilder)
+  // 右键菜单
+  const contextMenu = Menu.buildFromTemplate(
+    menu_edit
+      .concat(menu_tool)
+      .concat(menu_page)
+  );
+  win.webContents.on('context-menu', (e) => {
+    e.preventDefault();
+    contextMenu.popup(win);
+  });
 })
-// 当所有的窗口被关闭后退出应用
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') { app.quit() }
-})
+app.on('window-all-closed', () => app.quit());
