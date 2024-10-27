bda032dc1437b2dffd330005b2278e2f20ed849a
YCL0609 <YCL20060609@163.com>
2024-10-27 04:04:24 -0300

    fix bug

--- main.js
+++ main.js
@@ -40,39 +40,59 @@ const menu_edit = [{
 
 // 菜单-页面
 const menu_page = [{
-  label: '刷新',
+  label: '控制...',
+  submenu: [{
+    label: '开关右键菜单',
+    accelerator: 'F7',
+    click: () => {
+      const contextMenu = Menu.buildFromTemplate(
+        menu_edit
+          .concat(menu_tool)
+          .concat(menu_page)
+      );
+      BrowserWindow.getFocusedWindow().webContents.on('context-menu', (e) => {
+        e.preventDefault();
+        contextMenu.popup(contextMenu);
+      });
+    }
+  }, {
+    label: '显示当前网址',
+    accelerator: 'F10',
+    click: () => {
+      const url = BrowserWindow.getFocusedWindow().webContents.getURL();
+      const newwin = new BrowserWindow({ width: 500, height: 200 });
+      newwin.setMenu(null);
+      newwin.loadURL('data:text/html,<meta charset="UTF-8"><title>当前网址</title><body style=word-wrap:break-word;word-break:break-all>' + url)
+    }
+  }, {
+    label: '全屏',
+    accelerator: 'F11',
+    role: 'toggleFullScreen'
+
+  }, {
+    label: '开发者工具',
+    accelerator: 'F12',
+    role: 'toggleDevTools'
+  }, {
+    label: '关闭菜单(全局)',
+    click: () => { Menu.setApplicationMenu(null) }
+  }]
+}, {
+  label: '刷新(F5)',
   accelerator: 'F5',
   role: 'reload'
 }, {
-  label: '忽略缓存刷新',
+  label: '忽略缓存刷新(shift+F5)',
   accelerator: 'shift+F5',
   role: 'forceReload'
 }, {
-  label: '在独立线程中打开',
+  label: '在独立线程中打开(F9)',
   accelerator: 'F9',
   click: () => {
     const url = BrowserWindow.getFocusedWindow().webContents.getURL();
     const newwin = new BrowserWindow({ width: 1024, height: 600 });
     newwin.loadURL(url);
   }
-}, {
-  label: '显示当前网址',
-  accelerator: 'F10',
-  click: () => {
-    const url = BrowserWindow.getFocusedWindow().webContents.getURL();
-    const newwin = new BrowserWindow({ width: 500, height: 200 });
-    newwin.setMenu(null);
-    newwin.loadURL('data:text/html,<meta charset="UTF-8"><title>当前网址</title><body style=word-wrap:break-word;word-break:break-all>' + url)
-  }
-}, {
-  label: '全屏',
-  accelerator: 'F11',
-  role: 'toggleFullScreen'
-
-}, {
-  label: '开发者工具',
-  accelerator: 'F12',
-  role: 'toggleDevTools'
 }];
 
 app.setPath('userData', process.cwd() + "\\resources\\Data");// 用户配置文件夹
@@ -90,19 +110,7 @@ app.whenReady().then(() => {
     menu_edit
       .concat(menu_tool)
       .concat(menu_page)
-      .concat({ label: '关闭菜单(全局)', click: () => { Menu.setApplicationMenu(null) } })
   )
   Menu.setApplicationMenu(titleMenu);
-
-  // 右键菜单
-  const contextMenu = Menu.buildFromTemplate(
-    menu_edit
-      .concat(menu_tool)
-      .concat(menu_page)
-  );
-  win.webContents.on('context-menu', (e) => {
-    e.preventDefault();
-    contextMenu.popup(win);
-  });
 })
 app.on('window-all-closed', () => app.quit());
